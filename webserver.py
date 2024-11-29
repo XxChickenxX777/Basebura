a19510d5cc4a80c0ec6c13758713deedb0f9d967
XxChickenxX777 <cmaldonadoa2300@alumno.ipn.mx>
2024-11-29 00:41:39 -0600

    Admincuentas

--- webserver.py
+++ webserver.py
@@ -1,8 +1,4 @@
-from flask import Flask, render_template
-from flask_font_awesome import FontAwesome
-
-app = Flask(__name__)
-font_awesome = FontAwesome(app)
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask("BaseBura")
 
@@ -17,6 +13,20 @@ def cuentas():
 @app.route("/Administrador")
 def admin():
     return render_template("admin.html")
+
+@app.route("/f", methods=["GET", "POST"])
+def admino():
+    if request.method == "POST":
+        email = request.form.get("email")
+        #procesar datos de entrada
+        return redirect(url_for("admincuentas", email= email))
+    return render_template("admin.html")
+
+@app.route("/Administrar_cuentas")
+def admincuentas():
+    #obtener el parametro opcional
+    email = request.args.get("email", "contraseña")
+    return render_template("admincuentas.html", email=email)
 
 if __name__ == "__main__":
     app.run(debug=True)
